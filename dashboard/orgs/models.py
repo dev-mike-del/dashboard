e2fe69dcc4ac812a0ce9ac5a7c9669178ba337b1
dev-mike-del <delgado.portland@gmail.com>
2019-02-19 17:37:25 -0800

    Add Point and Detail models to orgs app

--- dashboard/orgs/models.py
+++ dashboard/orgs/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+
+class Point(models.Model):
+    title = models.CharField(max_length=30)
+    body = models.TextField()
+
+    def __str__(self):
+        return self.title
+
+
+class Detail(models.Model):
+    title = models.CharField(max_length=30)
+    body = models.TextField()
+    points = models.ManyToManyField(Point, related_name='points')
+
+    def __str__(self):
+        return self.title
