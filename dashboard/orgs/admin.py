8694694ad75d073b6748318893f1bb69528776ef
dev-mike-del <delgado.portland@gmail.com>
2019-02-21 16:23:34 -0800

    Add admin for orgs app

--- dashboard/orgs/admin.py
+++ dashboard/orgs/admin.py
@@ -1,3 +1,25 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import (Point,
+                     Detail,
+                     Feature,
+                     Item,
+                     Category,
+                     SubGroup,
+                     Group,
+                     SubProperty,
+                     Property,
+                     Organization
+                     )
+
+
+admin.site.register(Point)
+admin.site.register(Detail)
+admin.site.register(Feature)
+admin.site.register(Item)
+admin.site.register(Category)
+admin.site.register(SubGroup)
+admin.site.register(Group)
+admin.site.register(SubProperty)
+admin.site.register(Property)
+admin.site.register(Organization)
